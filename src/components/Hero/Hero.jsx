6b5a65718a5ab8aa0de647c992e14928f77958a2
Javier Quinteros <javieratnora@gmail.com>
2021-02-23 18:06:46 -0300

    Fade effect removed from hero.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,5 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { Container } from 'react-bootstrap';
-import Fade from 'react-reveal/Fade';
 import { Link } from 'react-scroll';
 import PortfolioContext from '../../context/context';
 
@@ -8,22 +7,9 @@ const Header = () => {
   const { hero } = useContext(PortfolioContext);
   const { title, name, subtitle, cta } = hero;
 
-  const [isDesktop, setIsDesktop] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    if (window.innerWidth > 769) {
-      setIsDesktop(true);
-      setIsMobile(false);
-    } else {
-      setIsMobile(true);
-      setIsDesktop(false);
-    }
-  }, []);
-
   return (
     <section id="hero" className="jumbotron">
-      <Container>
+      <Container style={{ paddingLeft: '5%' }}>
         <div className="d-flex justify-content-center">
           <h1 className="hero-title text-left">
             {title || 'Primero que nada...'}{' '}
@@ -32,15 +18,13 @@ const Header = () => {
             {subtitle || 'Buscabas un Desarrollador Fullstack?'}
           </h1>
         </div>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
-          <p className="hero-cta">
-            <span className="cta-btn cta-btn--hero">
-              <Link to="about" smooth duration={1000}>
-                {cta || 'NO hagas click aquí!'}
-              </Link>
-            </span>
-          </p>
-        </Fade>
+        <p className="hero-cta">
+          <span className="cta-btn cta-btn--hero">
+            <Link to="about" smooth duration={500}>
+              {cta || 'NO hagas click aquí!'}
+            </Link>
+          </span>
+        </p>
       </Container>
     </section>
   );
