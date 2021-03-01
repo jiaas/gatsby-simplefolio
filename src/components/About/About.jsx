d1a8814c02297055ee8d16341866c5de37909b8d
Javier Ignacio Quinteros <javier@nora.cl>
2021-03-01 19:18:49 -0300

    Fixed the styling padding from titles

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,92 +10,94 @@ const About = () => {
   const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
 
   return (
-    <section id="about" style={{ paddingTop: '10%' }}>
-      <Title title="¿Quién soy?" />
+    <section id="about">
       <Container>
-        <Row className="about-wrapper" style={{ paddingTop: '7%' }}>
-          <Col md={6} sm={12}>
-            <div className="about-wrapper__image">
-              <Card style={{ color: '#272341', padding: '5%' }}>
-                <Card.Body>
-                  <Row>
-                    <Col xs={12} md={12} lg={12}>
-                      <AboutImg alt="profile picture" filename={img} />
-                      <div style={{ paddingTop: '5%', paddingBottom: '6%' }}>
-                        <h1>Javier Ignacio</h1>
-                        <h1>Quinteros Carvajal</h1>
-                      </div>
-                      <Badge
-                        pill
-                        style={{
-                          backgroundColor: '#272341',
-                          color: '#fff',
-                          paddingTop: '6%',
-                          paddingBottom: '2%',
-                          paddingLeft: '10%',
-                          paddingRight: '10%',
-                          fontWeight: 'normal !important',
-                        }}
-                      >
-                        <h3>Desarrollador Fullstack</h3>
-                      </Badge>
-                    </Col>
-                  </Row>
-                  <Row style={{ paddingTop: '15%', paddingBottom: '3%' }}>
-                    <Col>
-                      <a
-                        href="https://github.com/cobidev/gatsby-simplefolio"
-                        rel="noopener noreferrer"
-                        target="_blank"
-                      >
-                        <i className="fa fa-linkedin fa-inverse" style={{ color: '#272341' }} />{' '}
-                      </a>
-                    </Col>
-                    <Col>
-                      <a
-                        href="https://github.com/cobidev/gatsby-simplefolio"
-                        rel="noopener noreferrer"
-                        target="_blank"
-                      >
-                        <i className="fa fa-twitter fa-inverse" style={{ color: '#272341' }} />{' '}
-                      </a>
-                    </Col>
-                    <Col>
-                      <a
-                        href="https://github.com/cobidev/gatsby-simplefolio"
-                        rel="noopener noreferrer"
-                        target="_blank"
-                      >
-                        <i className="fa fa-github fa-inverse" style={{ color: '#272341' }} />{' '}
-                      </a>
-                    </Col>
-                  </Row>
-                </Card.Body>
-              </Card>
-            </div>
-          </Col>
-          <Col md={6} sm={12}>
-            <Row style={{ padding: '5%' }}>
-              <div>
-                <p style={{ textAlign: 'justify' }}>{paragraphOne}</p>
-                <p style={{ textAlign: 'justify' }}>{paragraphTwo}</p>
-                <p style={{ textAlign: 'justify' }}>{paragraphThree}</p>
-                {resume && (
-                  <span className="d-flex mt-3">
-                    <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="cta-btn cta-btn--resume"
-                      href={resume}
-                    >
-                      CV
-                    </a>
-                  </span>
-                )}
+        <div className="about-wrapper">
+          <Title title="¿Quién soy?" />
+          <Row className="about-wrapper">
+            <Col md={6} sm={12}>
+              <div className="about-wrapper__image">
+                <Card style={{ color: '#272341', padding: '5%' }}>
+                  <Card.Body>
+                    <Row>
+                      <Col xs={12} md={12} lg={12}>
+                        <AboutImg alt="profile picture" filename={img} />
+                        <div style={{ paddingTop: '5%', paddingBottom: '6%' }}>
+                          <h1>Javier Ignacio</h1>
+                          <h1>Quinteros Carvajal</h1>
+                        </div>
+                        <Badge
+                          pill
+                          style={{
+                            backgroundColor: '#272341',
+                            color: '#fff',
+                            paddingTop: '6%',
+                            paddingBottom: '2%',
+                            paddingLeft: '10%',
+                            paddingRight: '10%',
+                            fontWeight: 'normal !important',
+                          }}
+                        >
+                          <h3>Desarrollador Fullstack</h3>
+                        </Badge>
+                      </Col>
+                    </Row>
+                    <Row style={{ paddingTop: '15%', paddingBottom: '3%' }}>
+                      <Col>
+                        <a
+                          href="https://github.com/cobidev/gatsby-simplefolio"
+                          rel="noopener noreferrer"
+                          target="_blank"
+                        >
+                          <i className="fa fa-linkedin fa-inverse" style={{ color: '#272341' }} />{' '}
+                        </a>
+                      </Col>
+                      <Col>
+                        <a
+                          href="https://github.com/cobidev/gatsby-simplefolio"
+                          rel="noopener noreferrer"
+                          target="_blank"
+                        >
+                          <i className="fa fa-twitter fa-inverse" style={{ color: '#272341' }} />{' '}
+                        </a>
+                      </Col>
+                      <Col>
+                        <a
+                          href="https://github.com/cobidev/gatsby-simplefolio"
+                          rel="noopener noreferrer"
+                          target="_blank"
+                        >
+                          <i className="fa fa-github fa-inverse" style={{ color: '#272341' }} />{' '}
+                        </a>
+                      </Col>
+                    </Row>
+                  </Card.Body>
+                </Card>
               </div>
-            </Row>
-          </Col>
-        </Row>
+            </Col>
+            <Col md={6} sm={12}>
+              <Row style={{ padding: '5%' }}>
+                <div>
+                  <p style={{ textAlign: 'justify' }}>{paragraphOne}</p>
+                  <p style={{ textAlign: 'justify' }}>{paragraphTwo}</p>
+                  <p style={{ textAlign: 'justify' }}>{paragraphThree}</p>
+                  {resume && (
+                    <span className="d-flex mt-3">
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="cta-btn cta-btn--resume"
+                        href={resume}
+                      >
+                        CV
+                      </a>
+                    </span>
+                  )}
+                </div>
+              </Row>
+            </Col>
+          </Row>
+        </div>
       </Container>
     </section>
   );
